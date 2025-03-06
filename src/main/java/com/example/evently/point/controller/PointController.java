b2f3baeff216b22b3592de37d0045e1d59b5a7b9
2-seulgi <cooey12@gmail.com>
2025-03-06 15:16:51 +0900

    redis 이용한 포인트 조회

--- src/main/java/com/example/evently/point/controller/PointController.java
+++ src/main/java/com/example/evently/point/controller/PointController.java
@@ -1,4 +1,26 @@
 package com.example.evently.point.controller;
 
+import com.example.evently.point.service.PointService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class PointController {
+
+    private final PointService pointService;
+
+    @GetMapping("/{userSn}/points")
+    public ResponseEntity<Integer> getUserPoints(@PathVariable Long userSn) {
+        int points = pointService.getUserPoints(userSn);
+        return ResponseEntity.ok(points);
+
+    }
+
+
 }
--- src/main/java/com/example/evently/point/service/PointService.java
+++ src/main/java/com/example/evently/point/service/PointService.java
@@ -6,6 +6,7 @@ import com.example.evently.user.domain.User;
 import com.example.evently.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -30,12 +31,30 @@ public class PointService {
 
         //2. 사용자 포인트 업데이트
         user = userRepository.findById(user.getId())
-                .orElseThrow(() -> new IllegalArgumentException("사용자 없음"));
+                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
         user.updatePoints(user.getPoints() + points);
 
         // 3. redis 캐싱 업데이트로 조회 속도 향상
         String key = POINT_CACHE_KEY + user.getId();
         redisTemplate.opsForValue().set(key, user.getPoints(), Duration.ofMinutes(5)); //이 데이터는 5분 후 자동 삭제
-
     }
+
+    @Cacheable(key = "'user:points:' + #userSn", unless = "#result == 0")
+    public int getUserPoints(Long userSn){
+        String key = POINT_CACHE_KEY + userSn;
+        Integer cachedPoints  = (Integer) redisTemplate.opsForValue().get(key);
+        if (cachedPoints != null) {
+            return cachedPoints;
+        }
+
+        // redis에 데이터가 없으면 db 조회
+        User user = userRepository.findById(userSn).orElseThrow(()-> new IllegalStateException("사용자를 찾을 수 없습니다."));
+        int points = user.getPoints();
+
+        // redis에 저장
+        redisTemplate.opsForValue().set(key, points, Duration.ofMinutes(5));
+
+        return points;
+    }
+
 }
