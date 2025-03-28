b099e39d8d3f01e4beb0e77a6bc974b39e0785bc
2-seulgi <cooey12@gmail.com>
2025-03-28 17:47:12 +0900

    아이디 중복 검사 및 유저롤 디í폴 트 설정

--- src/main/java/com/example/evently/auth/controller/AuthController.java
+++ src/main/java/com/example/evently/auth/controller/AuthController.java
@@ -4,6 +4,7 @@ import com.example.evently.auth.dto.LoginRequestDto;
 import com.example.evently.auth.service.AuthService;
 import com.example.evently.user.dto.UserRequestDto;
 import com.example.evently.user.dto.UserResponseDto;
+import com.example.evently.user.repository.UserRepository;
 import com.example.evently.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -20,6 +22,8 @@ public class AuthController {
 
     private final AuthService authService;
     private final UserService userService;
+    private final UserRepository userRepository;
+
 
     /**
      * 로그인 api
@@ -41,4 +45,18 @@ public class AuthController {
         UserResponseDto responseDto = authService.registerUser(userRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
     }
+
+    /**
+     * 중복 체크
+     * @param email
+     * @return
+     */
+    @GetMapping("/check-email")
+    public ResponseEntity<Map<String, Boolean>> checkEmailDuplicate(@RequestParam String email) {
+        boolean exists = userRepository.existsByUserId(email);
+        Map<String, Boolean> result = new HashMap<>();
+        result.put("duplicate", exists);
+        return ResponseEntity.ok(result);
+    }
+
 }
--- src/main/java/com/example/evently/user/dto/UserRequestDto.java
+++ src/main/java/com/example/evently/user/dto/UserRequestDto.java
@@ -9,7 +9,6 @@ public record UserRequestDto(
         @NotBlank(message = "아이디는 필수입니다.") String userId,
         @NotBlank(message = "이름은 필수입니다.") String userName,
         @NotNull @Size(min = 6, message = "비밀번호는 최소 6자 이상이어야 합니다.") String password,
-        @NotNull UserRole userRole
 ) {
 
 }
